e388a5d3dea43ae85e3d20dac7e1a4649d80ce3c
Jarrad <bluejay.jzp@gmail.com>
2023-09-23 15:03:48 +1000

    Added stylesheet to error page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,12 +1,17 @@
+import Page from "./Page";
+import "@/styleSheets/ErrorPage.css";
+
 function ErrorPage({ error, resetErrorBoundary }) {
     console.error(error);
 
     return (
-        <div className="errorPage">
-            <p>An error occurred:</p>
-            <pre>{error.message}</pre>
-            <button onClick={() => resetErrorBoundary()}>Try Again?</button>
-        </div>
+        <Page>
+            <div className="errorPage">
+                <p>An error occurred:</p>
+                <pre>{error.message}</pre>
+                <button onClick={() => resetErrorBoundary()}>Try Again?</button>
+            </div>
+        </Page>
     );
 }
 
