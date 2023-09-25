d2c3c3814b0206e4d0f6b73257ae08981198dde9
Jarrad <bluejay.jzp@gmail.com>
2023-09-26 07:51:34 +1000

    card style change

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,9 +1,10 @@
 import "./Card.css";
 
-export function Card() {
+export function Card({title, image}) {
     return (
         <div className="card">
-            
+            <img className="card__image" src={image}/>
+            <span className="card__title">{title}</span>
         </div>
     )
 }
