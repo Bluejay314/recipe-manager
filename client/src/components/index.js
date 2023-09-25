b8c6a62cbbee664b139fa9dc55cbe7e1a8654eee
Jarrad <bluejay.jzp@gmail.com>
2023-09-26 07:53:51 +1000

    set up export

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,5 @@
 export { BackgroundImage } from "./BackgroundImage/BackgroundImage"
+export { Card } from "./Card/Card";
 export { Header } from "./Header/Header"
 export { NavBar } from "./NavBar/NavBar"
 export { SearchBar } from "./SearchBar/SearchBar"
