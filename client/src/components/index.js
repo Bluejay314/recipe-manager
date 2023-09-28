a9cb637f3c6b4a6504ba2a65f500c0786e35ad8e
Jarrad <bluejay.jzp@gmail.com>
2023-09-28 16:20:16 +1000

    removed unused import

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,8 @@
-export { BackgroundImage } from "./BackgroundImage"
+export { CreateAccountForm } from "./CreateAccountForm"
 export { CollapsableMenu } from "./CallapsableMenu"
 export { RecipeCard } from "./RecipeCard";
 export { Header } from "./Header"
 export { Logo } from "./Logo"
 export { NavBar } from "./NavBar"
 export { SearchBar } from "./SearchBar"
+export { SignInForm } from "./SignInForm"
