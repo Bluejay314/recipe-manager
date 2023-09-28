3a6df036054eb14bec3e1179efad277c8bffd9b0
Jarrad <bluejay.jzp@gmail.com>
2023-09-28 11:54:36 +1000

    now uses env variables

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
@@ -7,6 +9,6 @@ app.get("/", (req, res) => {
     res.json({ message: "success" });
 });
 
-app.listen(3010, () => {
-    console.log(`Server is running on http://localhost:${3010}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
