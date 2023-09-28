b9ac91f46018e474272ac843ab3e1ba6a99435ff
Jarrad <bluejay.jzp@gmail.com>
2023-09-29 09:44:22 +1000

    fixed wrong controller function called

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,10 @@
 let express = require("express");
-let { userController } = require("../controllers")
+let { userController } = require("../controllers");
 
 let router = express.Router();
 
 router.get("/:id", (req, res) => userController.getUser(req, res));
-router.post("/", (req, res) => userController.createUser(req.body, res));
+router.post("/", (req, res) => userController.registerUser(req, res));
 router.put('/:id', (req, res) => userController.updateUser(req, res));
 router.delete("/:id", (req, res) => userController.deleteUser(req, res));
 
