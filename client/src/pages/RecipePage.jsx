5978e081ed5b193c35bfa62b49a680be8a0fbb3b
Jarrad <bluejay.jzp@gmail.com>
2023-09-28 10:09:55 +1000

    Added styling to the page

--- client/src/pages/RecipePage.jsx
+++ client/src/pages/RecipePage.jsx
@@ -1,43 +1,65 @@
 import useQuery from "@/hooks/useQuery";
 import { Page } from "@/pages";
 import recipes from "../data/allrecipes";
-import { Box, Card, Typography } from "@mui/material";
+import { Box, Card, Grid, Typography } from "@mui/material";
 import { SideBar } from "@/components";
 
 export function RecipePage() {
     const query = useQuery();
     const recipeId = query.get("q");
-    const recipe = recipes.find(r => r.id == recipeId);
+    const recipe = recipes.find((r) => r.id == recipeId);
 
-    const ingredients = recipe.ingredients.map(ing => (
+    const ingredients = recipe.ingredients.map((ing) => (
         <Box display="flex" alignItems="center">
             <Typography variant="body1">
                 {`${ing.quantity} ${ing.unit} ${ing.name}`}
-            </Typography>  
+            </Typography>
         </Box>
     ));
 
     return (
         <Page>
-            <Box sx={{
-                display: "grid",
-                gridTemplateColumns: "20% 80%",
-                gap: "2em",
-                py: "2em"
-            }}>
-                <SideBar />
-                <Box >
-                    <Typography variant="h2">{recipe.title}</Typography>
-                    <Typography variant="body1">{recipe.description}</Typography>
-                    {/* <img src={recipe.image} /> */}
+            <Grid container>
+                <Grid item md={3} sx={{display: { sm: "none", md: "block" }}}>
+                    <SideBar />
+                </Grid>
 
-                    <Typography variant="h6">Ingredients</Typography>
-                    {ingredients}
+                <Grid item container md p={4}>
+                    {/* Image Section */}
+                    <Grid item sm={12} md={5}>
+                        <img src={recipe.image} />
+                    </Grid>
 
-                    <Typography variant="h6">Method</Typography>
-                    <Typography variant="body1">{recipe.method}</Typography>
-                </Box>
-            </Box>
+                    {/* Header Section */}
+                    <Grid item sm={12} md={7}>
+                        <Typography variant="h3">{recipe.title}</Typography>
+                        <Typography variant="body1">{recipe.description}</Typography>
+                    </Grid>
+
+                    {/* Ingredients Section */}
+                    <Grid item sm={12} md>
+                        <Typography variant="h6">Ingredients</Typography>
+                        {ingredients}
+                    </Grid>
+
+                    {/* Method Section */}
+                    <Grid item sm={12} md>
+                        <Typography variant="h6">Method</Typography>
+                        <Typography variant="body1">
+                            {recipe.method.map((step, i) => (
+                                <Box key={i} pb={2}>
+                                    <Typography variant="subtitle1" fontWeight={600}>
+                                        Step {i + 1}
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        {step}
+                                    </Typography>
+                                </Box>
+                            ))}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
         </Page>
-    )
+    );
 }
