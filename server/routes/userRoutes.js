a9c3af17f826029936e4cd0855eb736602d3eace
Jarrad <bluejay.jzp@gmail.com>
2023-09-29 14:29:13 +1000

    Seperate routes for login and register

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,7 +4,8 @@ let { userController } = require("../controllers");
 let router = express.Router();
 
 router.get("/:id", (req, res) => userController.getUser(req, res));
-router.post("/", (req, res) => userController.registerUser(req, res));
+router.post("/register", (req, res) => userController.registerUser(req, res));
+router.post("/login", (req, res) => userController.loginUser(req, res));
 router.put('/:id', (req, res) => userController.updateUser(req, res));
 router.delete("/:id", (req, res) => userController.deleteUser(req, res));
 
