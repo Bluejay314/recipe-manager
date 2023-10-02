0a92cbee42ba82213be473180887009054470600
Jarrad <bluejay.jzp@gmail.com>
2023-10-02 12:03:30 +1100

    added route for adding recipe image

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -1,11 +1,12 @@
 let express = require("express");
-let { recipeController } = require("../controllers")
+let { recipeController } = require("../controllers");
+const { uploadFile } = require("../middleware/uploads");
 
 let router = express.Router();
 
-router.get("/", (_, res) => recipeController.getRecipes(res));
 router.get("/:id", (req, res) => recipeController.getRecipe(req, res));
-router.post("/:id", (req, res) => recipeController.createRecipe(req.body, res));
+router.post("/", (req, res) => recipeController.createRecipe(req.body, res));
+router.post('/:userId/image/', uploadFile, (req, res) => recipeController.createRecipeImage(req, res));
 router.put("/:id", (req, res) => recipeController.updateRecipe(req, res));
 router.delete("/:id", (req, res) => recipeController.deleteRecipe(req, res));
 
