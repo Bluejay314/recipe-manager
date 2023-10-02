218fb7d90a8c4eb748d6294665afcf954089e3b4
Jarrad <bluejay.jzp@gmail.com>
2023-10-03 09:21:42 +1100

    succesfully uploads images

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -1,12 +1,14 @@
 let express = require("express");
 let { recipeController } = require("../controllers");
 const { uploadFile } = require("../middleware/uploads");
+const { verifyToken } = require("../middleware/auth");
 
 let router = express.Router();
 
 router.get("/:id", (req, res) => recipeController.getRecipe(req, res));
-router.post("/", (req, res) => recipeController.createRecipe(req.body, res));
-router.post('/:userId/image/', uploadFile, (req, res) => recipeController.createRecipeImage(req, res));
+router.get("/all/:id", verifyToken, (req, res) => recipeController.getUserRecipes(req, res));
+router.post("/:id",uploadFile, (req, res) => recipeController.createRecipe(req, res));
+router.put('/:id/image', uploadFile, (req, res) => recipeController.createRecipeImage(req, res));
 router.put("/:id", (req, res) => recipeController.updateRecipe(req, res));
 router.delete("/:id", (req, res) => recipeController.deleteRecipe(req, res));
 
