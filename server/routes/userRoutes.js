3eeb8b81838ec2c5cd8bab01d772d528482790fd
Jarrad <bluejay.jzp@gmail.com>
2023-10-03 09:21:49 +1100

    token verification

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,11 @@
 let express = require("express");
-let { userController } = require("../controllers");
+let { userController, recipeController } = require("../controllers");
+const { verifyToken } = require("../middleware/auth");
 
 let router = express.Router();
 
-router.get("/:id", (req, res) => userController.getUser(req, res));
 router.post("/register", (req, res) => userController.registerUser(req, res));
 router.post("/login", (req, res) => userController.loginUser(req, res));
-router.put('/:id', (req, res) => userController.updateUser(req, res));
-router.delete("/:id", (req, res) => userController.deleteUser(req, res));
+router.put('/:id', verifyToken, (req, res) => userController.updateUser(req, res));
 
 module.exports = router;
