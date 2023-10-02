01d02dc923d3b4b6cc4316988de22270eff6b632
Jarrad <bluejay.jzp@gmail.com>
2023-10-03 09:25:25 +1100

    added fetch all recipes of user

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let { Recipe } = require("../models");
+let { Recipe, User } = require("../models");
 
 const getRecipe = async (req, res) => {
     try {
@@ -8,17 +8,33 @@ const getRecipe = async (req, res) => {
         res.send({ result: 200, data: recipe });
     }
     catch(err) {
-        res.send({ result: 500, error: err.message })
+        res.send({ result: 500,  error: err.message })
     }
 };
 
-const createRecipe = async (data, res) => {
+const getUserRecipes = async (req, res) => {
     try {
-        const recipe = await new Recipe(data).save();
+        const recipe = await Recipe.find({user: req.params.id});
         res.send({ result: 200, data: recipe });
     }
     catch(err) {
-        res.send({ result: 500, error: err.message });
+        res.send({ result: 500,  error: err.message })
+    }
+};
+
+const createRecipe = async (req, res) => {
+    try {
+        let data = req.body;
+        if(req.file) 
+            data = {...data, image: '/images/' + req.file.filename};
+        
+            console.log(data)
+        const recipe = await new Recipe(data).save();
+
+        res.send({ result: 200, data: recipe });
+    }
+    catch(err) {
+        res.send({ result: 500, sss: req.body.data, error: err.message });
     }
 };
 
@@ -31,15 +47,13 @@ const updateRecipe = (req, res) => {
 };
 
 const createRecipeImage = (req, res) => {
-    const userUpdates = { 
+    const update = { 
         image: '/images/' + req.file.filename
     };
 
-    console.log(userUpdates);
-
-    User.findByIdAndUpdate(req.params.userId, userUpdates)
+    Recipe.findByIdAndUpdate(req.params.id, update)
         .then(() => 
-            res.status(200).json({ result: 'Image uploaded to profile successfully', data: userUpdates }))
+            res.status(200).json({ result: 'Image uploaded to profile successfully', data: update }))
         .catch(err => 
             res.status(500).json({ result: err.message })
     )
@@ -55,6 +69,7 @@ const deleteRecipe = (req, res) => {
 
 module.exports = {
     getRecipe,
+    getUserRecipes,
     createRecipeImage,
     createRecipe,
     updateRecipe,
