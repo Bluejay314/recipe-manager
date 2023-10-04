e78a301499da1abff54ca66f5b5e4ac1d145f8b9
Jarrad <bluejay.jzp@gmail.com>
2023-10-05 04:55:39 +1100

    updated tags component

--- client/src/components/recipeBuilder/RecipeDetailsForm.jsx
+++ client/src/components/recipeBuilder/RecipeDetailsForm.jsx
@@ -4,7 +4,7 @@ import { useRecipeBuildContext } from "@/context/RecipeBuildContext";
 
 export default function RecipeDetailsForm() {
     const { recipe, updateRecipe } = useRecipeBuildContext();
-    
+
     return (
         <Box px={"1em"}  display="flex" flexDirection="column" gap=".5em">
             <CssBaseline />
@@ -12,11 +12,9 @@ export default function RecipeDetailsForm() {
                     value={recipe.title} 
                     onChange={e => updateRecipe({title: e.target.value})} />
 
-
-                <TextField fullWidth label="Tags" name="title" 
-                    value={recipe.title} 
-                    onChange={e => updateRecipe({title: e.target.value})} />
-
+                <TextField fullWidth label="Tags" name="tags" 
+                    value={recipe.tags} 
+                    onChange={e => updateRecipe({tags: e.target.value})} />
 
                 <TextField name="description" label="description" multiline rows={3} fullWidth
                     value={recipe.description} 
