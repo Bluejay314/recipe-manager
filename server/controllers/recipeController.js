8a1f87f950d48d90920cedf2b9a9f82895fc6192
Jarrad <bluejay.jzp@gmail.com>
2023-10-05 05:22:38 +1100

    appropriated controller for front-end usage

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,63 +1,69 @@
 "use strict";
 
-let { Recipe, User } = require("../models");
+let { Recipe } = require("../models");
 
 const getRecipe = async (req, res) => {
     try {
         const recipe = await Recipe.findById(req.params.id);
+        
         res.send({ result: 200, data: recipe });
-    }
-    catch(err) {
-        res.send({ result: 500,  error: err.message })
+    } catch (err) {
+        res.send({ result: 500, error: err.message });
     }
 };
 
 const getUserRecipes = async (req, res) => {
     try {
-        const recipe = await Recipe.find({user: req.params.id});
+        let recipe;
+        if(req.query.amount) {
+            const limit = Number(req.query.amount);
+            recipe = await Recipe.find({ user: req.params.id }).limit(limit);
+        }
+        else 
+            recipe = await Recipe.find({ user: req.params.id });
         res.send({ result: 200, data: recipe });
-    }
-    catch(err) {
-        res.send({ result: 500,  error: err.message })
+    } catch (err) {
+        res.send({ result: 500, error: err.message });
     }
 };
 
 const createRecipe = async (req, res) => {
     try {
         let data = req.body;
-        if(req.file) 
-            data = {...data, image: '/images/' + req.file.filename};
-        
-            console.log(data)
+        if (req.file) data = { ...data, image: "/images/" + req.file.filename };
+
+        data.tags = data.tags.split(/\s*,\s*/);
         const recipe = await new Recipe(data).save();
 
         res.send({ result: 200, data: recipe });
-    }
-    catch(err) {
+    } catch (err) {
         res.send({ result: 500, sss: req.body.data, error: err.message });
     }
 };
 
 const updateRecipe = (req, res) => {
-    Recipe.findByIdAndUpdate(req.params.id, req.body, {
-        useFindAndModify: false,
-    })
+    console.dir(`body: ${req.body.title}`)
+    Recipe.findByIdAndUpdate(req.params.id, req.body)
         .then((data) => res.send({ result: 200, data: data }))
         .catch((err) => res.send({ result: 500, error: err.message }));
 };
 
 const createRecipeImage = (req, res) => {
-    const update = { 
-        image: '/images/' + req.file.filename
+    const update = {
+        image: "/images/" + req.file.filename,
     };
 
     Recipe.findByIdAndUpdate(req.params.id, update)
-        .then(() => 
-            res.status(200).json({ result: 'Image uploaded to profile successfully', data: update }))
-        .catch(err => 
-            res.status(500).json({ result: err.message })
-    )
-}
+        .then(() =>
+            res
+                .status(200)
+                .json({
+                    result: "Image uploaded to profile successfully",
+                    data: update,
+                })
+        )
+        .catch((err) => res.status(500).json({ result: err.message }));
+};
 
 const deleteRecipe = (req, res) => {
     Recipe.findByIdAndRemove(req.params.id, req.body, {
@@ -73,5 +79,5 @@ module.exports = {
     createRecipeImage,
     createRecipe,
     updateRecipe,
-    deleteRecipe
+    deleteRecipe,
 };
