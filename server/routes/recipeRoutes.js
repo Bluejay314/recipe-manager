407fe99deeb62be59d7a4bd703ad45f1326c14a7
Jarrad <bluejay.jzp@gmail.com>
2023-10-05 05:21:19 +1100

    reworded route paths

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -5,11 +5,14 @@ const { verifyToken } = require("../middleware/auth");
 
 let router = express.Router();
 
-router.get("/:id", (req, res) => recipeController.getRecipe(req, res));
 router.get("/all/:id", verifyToken, (req, res) => recipeController.getUserRecipes(req, res));
+router.get("/:id", (req, res) => recipeController.getRecipe(req, res));
+
 router.post("/:id",uploadFile, (req, res) => recipeController.createRecipe(req, res));
+
+router.put("/update/:id", (req, res) => recipeController.updateRecipe(req, res));
 router.put('/:id/image', uploadFile, (req, res) => recipeController.createRecipeImage(req, res));
-router.put("/:id", (req, res) => recipeController.updateRecipe(req, res));
+
 router.delete("/:id", (req, res) => recipeController.deleteRecipe(req, res));
 
 module.exports = router;
