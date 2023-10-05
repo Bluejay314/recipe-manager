7f49f71b58b31cea3151cfea27d0608ea438b125
Jarrad <bluejay.jzp@gmail.com>
2023-10-05 15:01:22 +1100

    added support for smaller screens

--- client/src/components/RecipeMenu.jsx
+++ client/src/components/RecipeMenu.jsx
@@ -21,21 +21,31 @@ import StarIcon from '@mui/icons-material/Star';
 import { useRecipeBuildContext } from "@/context/RecipeBuildContext";
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import SaveIcon from '@mui/icons-material/Save';
+import Switch from '@mui/material/Switch';
 import axios from "axios";
+import { AIAvatar } from ".";
 
 const drawerWidth = 300;
-const menu1 = ["Edit", "Delete", "export", "image"];
-const menu2 = ["Branch Recipe"];
 
-export default function RecipeMenu({ isMobile=false }) {
+export default function RecipeMenu() {
     const [mobileOpen, setMobileOpen] = useState(false);
+    const [botEnabled, setBotEnabled] = useState(false);
     const { recipe, updateRecipe, getUpdateFormData } = useRecipeBuildContext();
+    
     
     const handleDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
     };
 
-    const handleFavourite = () => {
+    const handleFavourite = async () => {
+        try {
+            const response = await axios.put(`http://localhost:3010/recipes/update/${recipe._id}`, {favourite: !recipe.favourite})
+            console.dir(response.body);
+        }
+        catch(err) {
+            console.log(err)
+        }
+
         updateRecipe({favourite: !recipe.favourite})
     }
 
@@ -57,6 +67,10 @@ export default function RecipeMenu({ isMobile=false }) {
         }
     }
 
+    const handleSaveAs = () => {
+        
+    }
+
     const drawer = (
         <div>
             <List>
@@ -67,81 +81,90 @@ export default function RecipeMenu({ isMobile=false }) {
                     </ListItemButton>
                 </ListItem>
                 <ListItem disablePadding>
+                    <ListItemButton onClick={handleSaveAs} disabled={!recipe.canEdit? true : false}>
+                        <ListItemIcon><ModeEditIcon /></ListItemIcon>
+                        <ListItemText primary="Save As" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
                     <ListItemButton onClick={handleCanEdit}>
                         <ListItemIcon><SaveIcon /></ListItemIcon>
                         <ListItemText primary="Edit" />
                     </ListItemButton>
                 </ListItem>
+
                 <ListItem disablePadding>
                     <ListItemButton onClick={handleFavourite}>
-                        <ListItemIcon>
-                            {recipe.favourite === true? <StarIcon color="error" /> : <StarBorderIcon />}
-                        </ListItemIcon>
+                        <ListItemIcon>{recipe.favourite === true? <StarIcon color="error" /> : <StarBorderIcon />}</ListItemIcon>
                         <ListItemText primary="Favourite" />
                     </ListItemButton>
                 </ListItem>
             </List>
+
             <Divider />
+            
             <List>
-                {menu2.map((text) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon><InboxIcon /></ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
+                <ListItem >
+                    <ListItemIcon><Switch  checked={botEnabled} onChange={() => setBotEnabled(!botEnabled)}/></ListItemIcon>
+                    <ListItemText primary="AI Enabled" />
+                </ListItem>
             </List>
         </div>
     );
 
     return (
-        <Box sx={{ display: "flex" }}>
+        <Box>
             <CssBaseline />
-            {isMobile? (
-                <Fragment>
-                    <IconButton
-                        color="inherit"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        sx={{ mr: 2, display: { sm: "none" } }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
+            {botEnabled && <AIAvatar />}
                     <Box
-                        component="nav"
-                        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-                        aria-label="mailbox folders"
-                    >
-                        <Drawer
-                            variant="temporary"
-                            open={mobileOpen}
-                            onClose={handleDrawerToggle}
-                            sx={{
-                                "& .MuiDrawer-paper": {
-                                    boxSizing: "border-box",
-                                    width: drawerWidth,
-                                },
-                            }}
-                        >
-                            {drawer}
-                        </Drawer>
-                    </Box>
-                </Fragment>
-            ) : (
-                <Fragment>
-                    <Paper
                         sx={{    
-                            display: { sm: "none", md: "block" },
                             width: drawerWidth,
                             height: "100vh",
+                            display: {xs: "none", md: "flex"}
                             }}
                         open
                     >
                         {drawer}
-                    </Paper>
-                </Fragment>
-            )}
+                    </Box>
+
+                {/* The following is for mobile devices or small screens */}
+                <Drawer
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block', md: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+
+                <IconButton
+                    color="black"
+                    edge="start"
+                    onClick={handleDrawerToggle}
+                    sx={{ 
+                        display: {xs: "block", md: "none"}, 
+                        alignItems: "center",
+                        color: "rgb(100, 64, 35)",
+                        justifyContent: "center",
+                        boxShadow: "0 0 4px rgb(100, 64, 35)",
+                        mr: 2, 
+                        border: "2px solid rgb(100, 64, 35)",
+                        aspectRatio: 1,
+                        position: "absolute",
+                        top: "5em",
+                        right: "1em",
+                        zIndex: 99
+                    }}
+                >
+                    <MenuIcon sx={{pb: "0.em",}} />
+                </IconButton>
         </Box>
     );
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { Header, RecipeCard } from "@/components";
 import NewCard from "@/components/NewCard";
 import { useRecipes } from "@/hooks/useRecipes";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Paper, Typography } from "@mui/material";
 
 export function HomePage() {
     const recipes = useRecipes(8);
@@ -15,42 +15,44 @@ export function HomePage() {
     const headerColor = "rgba(255, 162, 0,0.5)";
 
     return (
-       <Box display="flex" flexDirection="column" flexGrow="1" backgroundColor="rgba(60, 89, 99, 0.50)">
+       <Box display="flex" flexDirection="column" flexGrow="1"  backgroundColor="rgba(0, 0, 0, 0.2)">
             <Header />
-            <Box 
-                mx="20%" 
-                flexDirection="column" 
-                alignContent="center" 
-                py="4em"
-                px="2em" 
-                gap="4em" 
-                backgroundColor="white"
-                boxShadow="0px 0px 3px rgb(245, 144, 66)"
-            >
-                <Box pb="3em">
-                    <Typography pl={3} py="0.25em" mb="0.5em" variant="h5" fontFamily="Raleway" fontWeight={600} sx={{
-                        backgroundColor: headerColor
-                    }}>Recently Viewed</Typography>
-                    <Grid container gap="2em" justifyContent="flex-start">
-                        {recipeItems}
-                    </Grid>
-                </Box>
-                <Box pb="3em">
-                    <Typography pl={3} py="0.25em" mb="0.5em" variant="h5" fontFamily="Raleway" fontWeight={600} sx={{
-                        backgroundColor: headerColor
-                    }}>Favourites</Typography>
-                    <Grid container gap="2em" justifyContent="flex-start">
-                        {recipeItems}
-                    </Grid> 
-                </Box>
-                <Box pb="3em">
-                    <Typography pl={3} py="0.25em" mb="0.5em" variant="h5" fontFamily="Raleway" fontWeight={600} sx={{
-                        backgroundColor: headerColor
-                    }}>Recent</Typography>
-                    <Grid container gap="2em" justifyContent="flex-start">
-                        {recipeItems}
-                    </Grid>
-                </Box>
+            <Box  mx="22%">
+                <Paper elevation={8}>
+                        <Box 
+                            flexDirection="column" 
+                            alignContent="center" 
+                            py="4em"
+                            px="2em" 
+                            gap="4em" 
+                            backgroundColor="white"
+                        >
+                        <Box pb="3em">
+                            <Typography pl={3} py="0.25em" mb="0.5em" variant="h5" fontFamily="Raleway" fontWeight={600} sx={{
+                                backgroundColor: headerColor
+                            }}>Recently Viewed</Typography>
+                            <Grid container gap="2em" justifyContent="flex-start">
+                                {recipeItems}
+                            </Grid>
+                        </Box>
+                        <Box pb="3em">
+                            <Typography pl={3} py="0.25em" mb="0.5em" variant="h5" fontFamily="Raleway" fontWeight={600} sx={{
+                                backgroundColor: headerColor
+                            }}>Favourites</Typography>
+                            <Grid container gap="2em" justifyContent="flex-start">
+                                {recipeItems}
+                            </Grid> 
+                        </Box>
+                        <Box pb="3em">
+                            <Typography pl={3} py="0.25em" mb="0.5em" variant="h5" fontFamily="Raleway" fontWeight={600} sx={{
+                                backgroundColor: headerColor
+                            }}>Recent</Typography>
+                            <Grid container gap="2em" justifyContent="flex-start">
+                                {recipeItems}
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Paper>
             </Box>
         </Box>
        
