57c96eea927916251fa54ab414cb844dded6808a
Jarrad <bluejay.jzp@gmail.com>
2023-10-05 14:58:03 +1100

    Added routes for using AI

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -6,10 +6,14 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_KEY
 });
 
-const getIngredientsChange = async (req, res) => {
+const model = "gpt-3.5-turbo";
+const temperature = 0.1;
+
+const ingredientsResponse = async (req, res) => {
     try {
-        const ingredients = req.body.ingredients;
+        const ingredients = req.body.content;
         const userRequest = req.body.userRequest;
+        console.dir(req.body)
 
         if(!ingredients) {
             res.send({result: 400, data: "ingredients is empty"});
@@ -27,27 +31,100 @@ const getIngredientsChange = async (req, res) => {
             ${ingredients}
             
             I want to ${userRequest}. can you modify my ingredients list
-            to do this? Only respond with a list of ingredients. give no explanation.`
+            to do this?`
         }
 
         const response = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
+            model: model,
             messages: [userMessage],
             max_tokens: 200,
-            temperature: 0,
+            temperature: temperature,
         });
 
-        return res.status(200).json({
-            message: "success",
-            data: response.choices[0].message
+        res.send({ result: 200, data: response.choices[0].message });
+    } catch (err) {
+        res.send({ result: 500, error: err.message });
+    }
+};
+
+const methodResponse = async (req, res) => {
+    try {
+        const content = req.body.content;
+        const userRequest = req.body.userRequest;
+        console.dir(req.body)
+
+        if(!content) {
+            res.send({result: 400, data: "method is empty"});
+            return;
+        }
+
+        if(!userRequest) {
+            res.send({result: 400, data: "No user request"});
+            return;
+        }
+
+        const userMessage = {
+            role: "user",
+            content: `I have the following method for a recipe:
+            ${content}
+            
+            I want to ${userRequest}. can you modify my list of steps
+            to do this?`
+        }
+
+        const response = await openai.chat.completions.create({
+            model: model,
+            messages: [userMessage],
+            max_tokens: 200,
+            temperature: temperature,
         });
         
-        res.send({ result: 200, data: recipe });
+        res.send({ result: 200, data: response.choices[0].message });
+    } catch (err) {
+        res.send({ result: 500, error: err.message });
+    }
+};
+
+const descriptionResponse = async (req, res) => {
+    try {
+        const content = req.body.content;
+        const userRequest = req.body.userRequest;
+        console.dir(req.body)
+
+        if(!content) {
+            res.send({result: 400, data: "description is empty"});
+            return;
+        }
+
+        if(!userRequest) {
+            res.send({result: 400, data: "No user request"});
+            return;
+        }
+
+        const userMessage = {
+            role: "user",
+            content: `I have the following description for a recipe:
+            "${content}"
+            
+            I want to ${userRequest}. can you modify the description
+            to do this?`
+        }
+
+        const response = await openai.chat.completions.create({
+            model: model,
+            messages: [userMessage],
+            max_tokens: 200,
+            temperature: temperature,
+        });
+        
+        res.send({ result: 200, data: response.choices[0].message });
     } catch (err) {
         res.send({ result: 500, error: err.message });
     }
 };
 
 module.exports = {
-    getIngredientsChange
+    ingredientsResponse,
+    methodResponse,
+    descriptionResponse
 };
--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -3,6 +3,8 @@ let { aiController } = require("../controllers");
 
 let router = express.Router();
 
-router.get("/ingredients", (req, res) => aiController.getIngredientsChange(req, res));
+router.post("/ingredients", (req, res) => aiController.ingredientsResponse(req, res));
+router.post("/method", (req, res) => aiController.methodResponse(req, res));
+router.post("/description", (req, res) => aiController.descriptionResponse(req, res));
 
 module.exports = router;
