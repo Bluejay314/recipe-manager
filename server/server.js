86f527003917231e7fb09a591b9572dd04314e6e
Jarrad <bluejay.jzp@gmail.com>
2023-10-06 07:57:53 +1100

    renamed to server.js

--- server/server.js
+++ server/server.js
@@ -9,9 +9,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors());
 app.use("/", express.static("public"));
 
 app.use("/recipes", recipeRoutes);
