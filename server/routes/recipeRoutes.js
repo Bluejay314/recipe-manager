6d03605957252accefac0d9d90a7161e85e44e5c
Jarrad <bluejay.jzp@gmail.com>
2023-10-06 15:01:56 +1100

    documentation

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -5,16 +5,28 @@ const { verifyToken } = require("../middleware/auth");
 
 let router = express.Router();
 
+// Get all recipes tied to a user
 router.get("/all/:id", verifyToken, (req, res) => recipeController.getUserRecipes(req, res));
+
+// Get all recipes tied to a user that matches the search parameter
 router.get("/:id/search/:q", (req, res) => recipeController.searchRecipes(req, res));
+
+// Get all recipes tied to a user marked as favourite
 router.get("/:id/favourites", (req, res) => recipeController.getFavourites(req, res));
+
+// Get one recipe with regards to its ID
 router.get("/:id", (req, res) => recipeController.getRecipe(req, res));
 
+// Create a new recipe for a user. If an image is present, perform /createRecipeImage' as well
 router.post("/:id",uploadFile, (req, res) => recipeController.createRecipe(req, res));
 
+// Get one recipe with regards to its ID and update with the body information
 router.put("/update/:id", (req, res) => recipeController.updateRecipe(req, res));
+
+// Upload an image to the public folder and add the path to the recipe image field
 router.put('/:id/image', uploadFile, (req, res) => recipeController.createRecipeImage(req, res));
 
+// Delete a recipe given its ID
 router.delete("/:id", (req, res) => recipeController.deleteRecipe(req, res));
 
 module.exports = router;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,8 +4,13 @@ const { verifyToken } = require("../middleware/auth");
 
 let router = express.Router();
 
+// Create a new user with information from the body
 router.post("/register", (req, res) => userController.registerUser(req, res));
+
+// Login a user with the details provided in the body
 router.post("/login", (req, res) => userController.loginUser(req, res));
+
+// Update details of a user. User must be logged in to update.
 router.put('/:id', verifyToken, (req, res) => userController.updateUser(req, res));
 
 module.exports = router;
